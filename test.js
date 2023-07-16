33229de7f250ffa02cd104e14d5bbdc07c0e4c0d
Electrochock1974 <114209777+Electrochock1974@users.noreply.github.com>
2023-07-16 23:14:11 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,16 +1,36 @@
 function calculateEasterSunday(year) {
   // Calcul des dates mobiles basées sur la date de Pâques.
-  const f = Math.floor,
-    G = year % 19,
-    C = f(year / 100),
-    H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30,
-    I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11)),
-    J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7,
-    L = I - J,
-    month = 3 + f((L + 40) / 44),
-    day = L + 28 - 31 * f(month / 4);
+// Math.floor arrondit un nombre à l'entier inférieur le plus proche.
+const f = Math.floor;
 
-  return new Date(year, month - 1, day);
+// G est le nombre d'années écoulées depuis le dernier cycle de 19 ans (aussi appelé cycle métanique).
+const G = year % 19;
+
+// C est le siècle.
+const C = f(year / 100);
+
+// H est une correction pour synchroniser le cycle lunaire.
+const H = (C - f(C / 4) - f((8 * C + 13) / 25) + 19 * G + 15) % 30;
+
+// I est une correction pour la règle "les années séculaires ne sont pas bissextiles".
+const I = H - f(H / 28) * (1 - f(29 / (H + 1)) * f((21 - G) / 11));
+
+// J est une correction pour synchroniser la semaine.
+const J = (year + f(year / 4) + I + 2 - C + f(C / 4)) % 7;
+
+// L est le nombre de jours depuis le 21 mars jusqu'à la pleine lune pascale.
+const L = I - J;
+
+// Le mois de Pâques (3 = Mars, 4 = Avril).
+const month = 3 + f((L + 40) / 44);
+
+// Le jour de Pâques.
+const day = L + 28 - 31 * f(month / 4);
+
+// La date de Pâques pour l'année donnée. Le mois est décrémenté de 1 car en JavaScript,
+// les mois sont comptés de 0 (Janvier) à 11 (Décembre).
+return new Date(year, month - 1, day);
+
 }
 
 function calculateFrenchHolidays(year) {
